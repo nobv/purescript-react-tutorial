b68838a460172dff93da1bff81824037030a5e14
nobv <6e6f6275@gmail.com>
2020-05-29 02:20:01 +0900

    hello world

--- src/Main.purs
+++ src/Main.purs
@@ -2,8 +2,18 @@ module Main where
 
 import Prelude
 import Effect (Effect)
-import Effect.Console (log)
+import Web.DOM.NonElementParentNode (getElementById)
+import Web.HTML.HTMLDocument (toNonElementParentNode)
+import Web.HTML.Window (document)
+import Web.HTML (window)
+import Data.Maybe (Maybe(..))
+import Effect.Exception (throw)
+import React.Basic.DOM (render)
+import Components.App (app)
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  root <- getElementById "root" =<< (map toNonElementParentNode $ document =<< window)
+  case root of
+    Nothing -> throw "Root element not found"
+    Just r -> render app r
