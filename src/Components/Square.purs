4c0c56f28d0bc81e04ee855d05c4b4f3d6d33b0a
nobv <6e6f6275@gmail.com>
2020-06-17 21:43:31 +0900

    Fixed to show value of props

--- src/Components/Square.purs
+++ src/Components/Square.purs
@@ -2,9 +2,9 @@ module Components.Square where
 
 import Prelude
 import Data.Foldable (fold)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe)
 import Effect (Effect)
-import React.Basic (Component, JSX, createComponent, make)
+import React.Basic (Component, JSX, createComponent, makeStateless)
 import React.Basic.DOM as R
 import React.Basic.Events (handler_)
 
@@ -13,17 +13,12 @@ type Props
     , onClick :: Effect Unit
     }
 
-component :: Component Unit
+component :: Component Props
 component = createComponent "square"
 
 square :: Props -> JSX
-square props = make component { initialState, render } unit
-  where
-  initialState =
-    { value: Nothing
-    }
-
-  render self =
+square =
+  makeStateless component \props ->
     R.button
       { className: "square"
       , children:
